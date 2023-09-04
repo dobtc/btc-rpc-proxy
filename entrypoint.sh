5cdf6bd278534c7725d86d4ae24ecf308940a240
Kroese <kroese@users.noreply.github.com>
2023-09-04 14:11:21 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,4 @@
 
 set -euo pipefail
 
-export HOST_IP=$(ip -4 route list match 0/0 | awk '{print $3}')
-
-exec tini btc-rpc-proxy --conf /etc/btc_rpc_proxy.toml
+exec tini /usr/local/bin/btc-rpc-proxy --conf /etc/btc_rpc_proxy.toml
